d49b8f7ca1853ad6672a4e6478e434881c31f9b6
SolomonB127 <solomonakinsulire@gmail.com>
2024-04-17 18:22:10 +0100

    Successfully displayed content of firstFile.txt to console.

--- src/FileModification.java
+++ src/FileModification.java
@@ -1,12 +1,11 @@
 //Importation of utilities.
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.io.*;
 
 public class FileModification {
     public static void main(String[] args) {
 //        Task 1: Creation of file & adding of content.
-        String content = "Hello, this  is a file handling project given by Aptech.";
+        String content = "Hello, this is a file handling project given by Aptech.";
 
         String contentReverse = new StringBuilder(content).reverse().toString();
         try {
@@ -17,7 +16,7 @@ public class FileModification {
             } else {
                 System.out.println("File already exists.");
             }
-//            Writing content into first file
+//            Writing content into first file.
             FileWriter writer = new FileWriter(firstFile);
             writer.write(content);
             writer.close();
@@ -30,7 +29,7 @@ public class FileModification {
             } else {
                 System.out.println("File already exist");
             }
-//            Writing content of file1 to file2(in reverse)
+//            Writing content of file1 to file2(in reverse).
             writer = new FileWriter(secondFile);
             writer.write(contentReverse);
             writer.close();
@@ -38,6 +37,17 @@ public class FileModification {
         } catch (IOException e) {
 //            Exception Handling
             System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+//        Task 3: Displaying content of first file to the console.
+        try (BufferedReader reader = new BufferedReader(new FileReader("firstFile.txt"))) {
+            String onCurrentLine;
+            while ((onCurrentLine = reader.readLine()) != null) {
+                System.out.println(onCurrentLine);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while trying to read file");
             e.printStackTrace();
         }
     }
